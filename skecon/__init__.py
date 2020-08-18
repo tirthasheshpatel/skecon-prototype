98d365e9707c01f33252693dbfe3b0077cb3bc09
Tirth Patel <tirthasheshpatel@gmail.com>
2020-08-18 13:46:38 +0530

    Add directory backbone

---
+++ skecon/__init__.py
@@ -0,0 +1,5 @@
+from .stats import *
+from .data import *
+
+__version__ = "0.0.1a1"
+
---
+++ skecon/stats.py
@@ -0,0 +1,9 @@
+
+# TODO: complete this code. Get it running and test it!!!
+
+import torch
+import numpy as np
+from scipy import stats
+
+def mean(x):
+    pass
